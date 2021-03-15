18753d4838fabbfafc2095d5002be4abe1d003be
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-03-15 15:30:19 +0100

    added tokens and utils

--- src/schemas/userModel.js
+++ src/schemas/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema(
     {
@@ -13,9 +14,43 @@ const userSchema = new mongoose.Schema(
                 "Please fill a valid email address",
             ]
         },
+        favourites: [{ scpId: { type: mongoose.Schema.Types.ObjectId, ref: "scps", required: true } }],
+        refreshToken: [{ token: { type: String } }],
         avatar: String,
-        password: { type: String, required: true }
+        password: { type: String, required: true },
+        role: {
+            type: String, required: true, enum: ["Admin", "User"]
+        }
     }
 )
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    delete userObject.__v;
+    delete userObject.refreshTokens;
+    return userObject;
+};
+
+userSchema.statics.findByCredentials = async function (email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) return user;
+        else return null;
+    } else {
+        return null;
+    }
+};
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+    const plainPW = user.password;
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(plainPW, 10);
+    }
+    next();
+});
 
 module.exports = mongoose.model("users", userSchema)
